a500698de1a5077bef2a8f2fe2e51d799c42c82d
Arthur Fakeev <RikyBone6@gmail.com>
2022-07-22 21:02:39 +0400

    Skeleton Blog

---
+++ articles/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.ArticleListView.as_view(), name='home'),
+    path('article/<int:pk>', views.ArticleDetailView.as_view(), name='detail'),
+
+]
